627d0dd5aea8c4e51cb2886dd15ff1420de51515
Christabel-Afia <143700778+Erphiyarh@users.noreply.github.com>
2024-05-26 13:57:18 +0000

    App.js

--- myApp/App.js
+++ myApp/App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        My name is <Text style={{ fontWeight: "bold" }}>Aboagye Christabel Afia Akwaboah</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FDDC5C",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 24,
+    color: "white",
+  },
+  text: {
+    fontSize: 24,
   },
 });
